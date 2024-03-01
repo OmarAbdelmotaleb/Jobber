2e60de123e899e17fad0b4367e88e4a83a2c1c28
Devkumar Patel <devpatel379.dp@gmail.com>
2024-03-01 07:50:13 +0530

    backend configuration and database connection

---
+++ __main__/myapp/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Item
+from .forms import ItemForm
+
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'myapp/item_list.html', {'items': items})
+
+def item_detail(request, pk):
+    item = Item.objects.get(pk=pk)
+    return render(request, 'myapp/item_detail.html', {'item': item})
+
+def item_create(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('item_list')
+    else:
+        form = ItemForm()
+    return render(request, 'myapp/item_form.html', {'form': form})
+
+def item_update(request, pk):
+    item = Item.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('item_list')
+    else:
+        form = ItemForm(instance=item)
+    return render(request, 'myapp/item_form.html', {'form': form})
+
+def item_delete(request, pk):
+    item = Item.objects.get(pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('item_list')
+    return render(request, 'myapp/item_confirm_delete.html', {'item': item})
+
