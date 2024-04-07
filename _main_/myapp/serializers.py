75fc783d739a239041211ca6c1047ec62d93d146
devp372 <Devpatel379.dp@gmail.com>
2024-04-07 22:48:27 +0530

    job application api

--- _main_/myapp/serializers.py
+++ _main_/myapp/serializers.py
@@ -1,11 +1,36 @@
 from rest_framework import serializers
-from .models import UserDetails
+from .models import UserDetails, Company, Job, Applications
 
 class UserDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserDetails
         fields = ['id', 'jobTitle', 'jobUrl', 'companyName', 'location', 'jobDescription']
 
-def get_serializer_class():
-    from .serializers import UserDetailsSerializer
-    return UserDetailsSerializer
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ['company_id', 'company_name', 'company_description']
+
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = ['job_id', 'job_title', 'job_url', 'company_id', 'job_location', 'job_description']
+    
+
+class ApplicationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Applications
+        fields = ['application_id', 'user_id', 'job_id']
+
+def get_serializer_class(model_name):
+     if model_name == 'UserDetails':
+        return UserDetailsSerializer
+     elif model_name == 'Company':
+        return CompanySerializer
+     elif model_name == 'Job':
+        return JobSerializer
+     elif model_name == 'Applications':
+        return ApplicationsSerializer    
+     else:
+        raise ValueError(f"No serializer found for model '{model_name}'")
--- _main_/myapp/urls.py
+++ _main_/myapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
-from .views import UserDetailsAPIView
+from .views import UserDetailsAPIView, CompanyListCreateAPIView, JobListCreateAPIView
 
 urlpatterns = [
     path('api/userdetails/create/', UserDetailsAPIView.as_view(), name='userdetails-create'),
     path('api/userdetails/<int:pk>/', UserDetailsAPIView.as_view(), name='userdetails-update'),
-    path('api/userdetails/', UserDetailsAPIView.as_view(), name='userdetails-list')
+    path('api/userdetails/', UserDetailsAPIView.as_view(), name='userdetails-list'),
+    path('companies/', CompanyListCreateAPIView.as_view(), name='company-list-create'),
+    path('companies/<int:pk>/', CompanyListCreateAPIView.as_view(), name='company-detail'),
+    path('jobs/', JobListCreateAPIView.as_view(), name='job-list-create'),
+    path('jobs/<int:pk>/', JobListCreateAPIView.as_view(), name='job-detail'),
 ]
