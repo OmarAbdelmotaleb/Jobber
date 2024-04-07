56ca01d6daf251391cf38157b62090c7627086d7
HinalThakrar <hinal.thakrar@chinmayfinlease.com>
2024-04-07 22:37:51 +0530

    job application api

--- _main_/myapp/models.py
+++ _main_/myapp/models.py
@@ -9,3 +9,31 @@ class UserDetails(models.Model):
 
     def __str__(self):
         return self.name  
+
+class Company(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    company_name = models.CharField(max_length=100)
+    company_description = models.TextField()
+
+    def __str__(self):
+        return self.company_name
+
+
+class Job(models.Model):
+    job_id = models.AutoField(primary_key=True)
+    job_title = models.CharField(max_length=100)
+    job_url = models.URLField()
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    job_location = models.CharField(max_length=100)
+    job_description = models.TextField()
+
+    def __str__(self):
+        return self.job_title
+
+class Applications(models.Model):
+    application_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(UserDetails, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Application {self.application_id}"
--- _main_/myapp/views.py
+++ _main_/myapp/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import UserDetails
-from .serializers import UserDetailsSerializer  # Importing serializer
+from .models import UserDetails, Company, Job
+from .serializers import UserDetailsSerializer, JobSerializer, CompanySerializer  # Importing serializer
 
 class UserDetailsAPIView(APIView):
     def get(self, request):
@@ -22,5 +22,50 @@ class UserDetailsAPIView(APIView):
         serializer = UserDetailsSerializer(userDetails, data=request.data)
         if serializer.is_valid():
             serializer.save()   
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+     # Add methods for Company CRUD operations
+class CompanyListCreateAPIView(APIView):
+    def get(self, request):
+        companies = Company.objects.all()
+        serializer = CompanySerializer(companies, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        company = Company.objects.get(pk=pk)
+        serializer = CompanySerializer(company, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+     # Add methods for Job CRUD operations
+class JobListCreateAPIView(APIView):
+    def get(self, request):
+        jobs = Job.objects.all()
+        serializer = JobSerializer(jobs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = JobSerializer(data=request.data)
+        print(serializer, 'check data>>>>>>..')
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        job = Job.objects.get(pk=pk)
+        serializer = JobSerializer(job, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
